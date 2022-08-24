d9602bea37fc863d5861cd1be298ab5e1caaa64a
重森建人 <shige960127@shigemokerusMBP.local>
2022-08-24 18:33:38 +0900

    SignUpのタブとInputが並列になりません

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -12,6 +12,7 @@ import { useTailwind } from "tailwind-rn/dist";
 import { handleSignUp } from "../stores/user";
 import { AppDispatch } from "../stores/index";
 import { useDispatch } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
 
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../App";
@@ -32,37 +33,57 @@ const SignUpScreen = ({ navigation }: Props) => {
       if (user) setUser({ ...user, [field]: e.nativeEvent.text });
     };
   return (
-    <SafeAreaView style={tailwind("flex-1 bg-stone-200")}>
-      <View style={tailwind("bg-white w-3/4 h-2/4 mt-20 ml-12 pt-2 pl-2")}>
-        <View style={tailwind("items-center m-4 p-4")}>
-          <Text style={tailwind("text-2xl font-bold")}>Sign Up</Text>
-        </View>
-        <View style={tailwind("")}>
-          <View>
-            <Text>Email:</Text>
+    <SafeAreaView style={tailwind("flex bg-violet-800")}>
+      <View style={tailwind("items-center m-4 p-4")}>
+        <Text style={tailwind("text-2xl font-bold text-white")}>
+          Create an Account!
+        </Text>
+      </View>
+      <View style={tailwind("flex bg-white my-4 py-4 h-full rounded-t-3xl")}>
+        <View style={tailwind("ml-1 pl-1")}>
+          <View style={tailwind("mt-4 ml-1 pt-4 pl-1")}>
+            <Text style={tailwind("text-lg font-bold")}>Email</Text>
+            <View style={tailwind("flex")}>
+              <View>
+                <FontAwesome name="user-circle-o" size={24} color="black" />
+              </View>
+              <View>
+                <TextInput
+                  style={tailwind("w-40 h-8 border rounded")}
+                  onChange={update("email")}
+                  value={user?.email}
+                  autoCapitalize={"none"}
+                  placeholder="E-mail"
+                />
+              </View>
+            </View>
           </View>
-          <TextInput
-            style={tailwind("w-40 border rounded")}
-            onChange={update("email")}
-            value={user?.email}
-            autoCapitalize={"none"}
-          />
-        </View>
-        <View>
-          <View>
-            <Text>Password:</Text>
+          <View style={tailwind("flex ml-1 pl-1")}>
+            <FontAwesome />
+            <Text style={tailwind("text-lg font-bold")}>Password</Text>
+            <TextInput
+              style={tailwind("w-80 h-8 border rounded")}
+              onChange={update("password")}
+              value={user?.password}
+              autoCapitalize={"none"}
+              placeholder="Password"
+              secureTextEntry
+            />
           </View>
-          <TextInput
-            style={tailwind("w-40 border rounded")}
-            onChange={update("password")}
-            value={user?.password}
-            autoCapitalize={"none"}
-            secureTextEntry
-          />
         </View>
-        <TouchableOpacity onPress={() => dispatch(handleSignUp({ ...user }))}>
-          <Text>サインアップする</Text>
-        </TouchableOpacity>
+        <View style={tailwind("items-center mt-4 pt-4")}>
+          <View
+            style={tailwind("items-center w-60 h-10 rounded-2xl bg-sky-400")}
+          >
+            <TouchableOpacity
+              onPress={() => dispatch(handleSignUp({ ...user }))}
+            >
+              <Text style={tailwind("text-white font-bold")}>
+                サインアップする
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
         <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
           <Text>すでにアカウントをお持ちの方はこちらをクリック</Text>
         </TouchableOpacity>
