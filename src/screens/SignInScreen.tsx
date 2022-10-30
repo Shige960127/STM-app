7d21a38d9b53282446a8897ca21772d2d2b9bfcb
重森建人 <shige960127@shigemokerusMBP.local>
2022-10-30 12:23:33 +0900

    [update]signin,signout

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -13,7 +13,7 @@ import { useDispatch } from "react-redux";
 import { handleSignIn } from "../stores/user";
 import { AppDispatch } from "../stores/index";
 import { AntDesign } from "@expo/vector-icons";
-// import LinearGradient from "expo-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 
 type Props = NativeStackScreenProps<RootStackParamList, "SignIn">;
 
@@ -24,79 +24,95 @@ const SignInScreen = ({ navigation }: Props) => {
   const [password, setPassword] = useState("");
 
   return (
-    <SafeAreaView style={tailwind("flex bg-violet-800")}>
-      <View style={tailwind("items-center m-4 p-4")}>
-        <Text style={tailwind("text-2xl font-bold text-white")}>
-          Hello Again!
-        </Text>
-      </View>
-      <View style={tailwind("flex bg-white my-4 py-4 h-full rounded-t-3xl")}>
-        <View style={tailwind("m-4")}>
-          <View>
-            <Text style={tailwind("text-lg font-bold")}>Email</Text>
-            <View style={tailwind("flex flex-row items-center")}>
-              <AntDesign
-                name="mail"
-                size={24}
-                color="black"
-                style={tailwind("mr-4")}
-              />
-              <View style={tailwind("flex-1")}>
-                <TextInput
-                  style={tailwind("w-full h-8 border rounded p-2")}
-                  onChange={(e) => setEmail(e.nativeEvent.text)}
-                  value={email}
-                  autoCapitalize={"none"}
-                  placeholder="E-mail"
+    <LinearGradient colors={["#17d9aa", "#3e17d9"]}>
+      <SafeAreaView style={tailwind("flex")}>
+        <View style={tailwind("items-center m-4 p-4")}>
+          <Text style={tailwind("text-2xl font-bold text-white mt-6")}>
+            今日も頑張りましょう！！
+          </Text>
+        </View>
+        <View style={tailwind("flex my-4 py-4 h-full rounded-t-3xl")}>
+          <View style={tailwind("m-4")}>
+            <View>
+              <View
+                style={tailwind(
+                  "flex flex-row items-center border-b-2 border-white m-1 p-1"
+                )}
+              >
+                <AntDesign
+                  name="mail"
+                  size={24}
+                  color="white"
+                  style={tailwind("mr-4")}
                 />
+                <View style={tailwind("flex-1")}>
+                  <TextInput
+                    style={tailwind("w-full h-8 p-2")}
+                    onChange={(e) => setEmail(e.nativeEvent.text)}
+                    value={email}
+                    autoCapitalize={"none"}
+                    placeholder="E-mail"
+                    placeholderTextColor={`#ffffff`}
+                  />
+                </View>
+              </View>
+            </View>
+            <View>
+              <View
+                style={tailwind(
+                  "flex flex-row items-center border-b-2 border-white m-1 p-1"
+                )}
+              >
+                <AntDesign
+                  name="lock"
+                  size={24}
+                  color="white"
+                  style={tailwind("mr-4")}
+                />
+                <View style={tailwind("flex-1")}>
+                  <TextInput
+                    style={tailwind("w-full h-8 p-2")}
+                    onChange={(e) => setPassword(e.nativeEvent.text)}
+                    value={password}
+                    autoCapitalize={"none"}
+                    placeholder="Password"
+                    secureTextEntry
+                    placeholderTextColor={`#ffffff`}
+                  />
+                </View>
               </View>
             </View>
           </View>
-          <View>
-            <Text style={tailwind("text-lg font-bold")}>Password</Text>
-            <View style={tailwind("flex flex-row items-center")}>
-              <AntDesign
-                name="lock"
-                size={24}
-                color="black"
-                style={tailwind("mr-4")}
-              />
-              <View style={tailwind("flex-1")}>
-                <TextInput
-                  style={tailwind("w-full h-8 border rounded p-2")}
-                  onChange={(e) => setPassword(e.nativeEvent.text)}
-                  value={password}
-                  autoCapitalize={"none"}
-                  placeholder="Password"
-                  secureTextEntry
-                />
-              </View>
-            </View>
+          <View style={tailwind("flex items-center justify-center mt-8")}>
+            <TouchableOpacity
+              onPress={() => dispatch(handleSignIn({ email, password }))}
+              style={tailwind(
+                "w-80 flex flex-row justify-center items-center p-4 rounded-2xl bg-sky-400"
+              )}
+            >
+              <Text style={tailwind("text-white font-bold")}>
+                サインインする
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={tailwind("text-white")}>
+                パスワードを忘れてしまった方はこちらをクリック
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SignUp")}
+              style={tailwind(
+                "w-80 flex flex-row justify-center items-center m-4 p-4 rounded-2xl bg-sky-400"
+              )}
+            >
+              <Text style={tailwind("text-white font-bold")}>
+                アカウント作成画面へ
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
-        <View style={tailwind("flex items-center justify-center mt-8")}>
-          <TouchableOpacity
-            onPress={() => dispatch(handleSignIn({ email, password }))}
-            style={tailwind(
-              "w-80 flex flex-row justify-center items-center p-4 rounded-2xl bg-sky-400"
-            )}
-          >
-            <Text style={tailwind("text-white font-bold")}>サインインする</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("SignUp")}
-            style={tailwind(
-              "w-80 flex flex-row justify-center items-center m-4 p-4 rounded-2xl bg-sky-400"
-            )}
-          >
-            <Text style={tailwind("text-white font-bold")}>
-              アカウント作成画面へ
-            </Text>
-          </TouchableOpacity>
-          {/* <GoogleSignIn /> */}
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
