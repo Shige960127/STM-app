c3752cab0286e6a027eb840d136099e4ca0c2f56
wdaitiw <idm00nbow@gmail.com>
2023-01-05 20:33:50 +0900

    install eas

--- app.config.js
+++ app.config.js
@@ -1,14 +1,16 @@
 import "dotenv/config";
 
-export default ({ config }) => {
-  config = { ...config };
-  config.extra = {
+export default ({ config }) => ({
+  ...config,
+  extra: {
+    eas: {
+      projectId: "e869c325-9092-44d7-91bb-e6359969500d",
+    },
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  };
-  return config;
-};
+  },
+});
