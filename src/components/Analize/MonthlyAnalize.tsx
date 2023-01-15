bba3edad89aab6649af25c2677274b8d11c5395b
wdaitiw <idm00nbow@gmail.com>
2023-01-15 22:11:58 +0900

    新しいbarchartを追加

--- src/components/Analize/MonthlyAnalize.tsx
+++ src/components/Analize/MonthlyAnalize.tsx
@@ -12,6 +12,7 @@ import {
   CategoryData,
   CreateChartData,
 } from "@components/Analize/Initial";
+import { BarChart } from "react-native-gifted-charts";
 
 const chartConfig = {
   backgroundGradientFrom: "black",
@@ -66,16 +67,102 @@ export default () => {
 
   if (!chartData) return <></>;
 
+  const primaries: string[] = [];
+
+  const stackMap = all.reduce(
+    (
+      prev: {
+        [key: string]: stack;
+      },
+      current
+    ) => {
+      const createAt = dateFormat(current.created_at.toDate(), "yyyy/MM");
+
+      if (!primaries.includes(current.primary_name))
+        primaries.push(current.primary_name);
+      prev[current.primary_id + createAt] = {
+        id: current.primary_id,
+        name: current.primary_name,
+        date: createAt,
+        value:
+          (prev[current.primary_id + createAt]?.value || 0) +
+          current.measuring_time,
+      };
+      return prev;
+    },
+    {}
+  );
+
+  const primaryClors = primaries.map((p) => {
+    const randomColor =
+      "rgb(" +
+      ~~(256 * Math.random()) +
+      ", " +
+      ~~(256 * Math.random()) +
+      ", " +
+      ~~(256 * Math.random()) +
+      ")";
+    return {
+      name: p,
+      color: randomColor,
+    };
+  });
+
+  const intermediateData: { [key: string]: stack[] } = Object.values(
+    stackMap
+  ).reduce((prev: { [key: string]: stack[] }, current) => {
+    const color = primaryClors.find((p) => p.name === current.name)?.color;
+    prev[current.date] = [
+      ...(prev[current.date] || []),
+      { ...current, color: color },
+    ];
+    return prev;
+  }, {});
+
+  const stackData: StackData[] = Object.entries(intermediateData).map((p) => {
+    return {
+      label: p[0],
+      stacks: p[1],
+    };
+  });
+
+  type StackData = {
+    stacks: stack[];
+    label: string;
+  };
+
+  type stack = {
+    id: string;
+    name: string;
+    date: string;
+    value: number;
+    color?: string;
+    marginBottom?: number;
+  };
+
   return (
     <>
-      <View style={tailwind("flex flex-row ")}>
-        <StackedBarChart
-          data={chartData}
-          height={(Dimensions.get("window").height * 4) / 5}
-          width={Dimensions.get("window").width}
-          chartConfig={chartConfig}
-          hideLegend={true}
+      <View style={tailwind("flex flex-row")}>
+        <BarChart
+          width={300}
+          height={500}
+          labelsExtraHeight={100}
+          noOfSections={4}
+          stackData={stackData}
         />
+        <View>
+          {primaryClors?.map((p) => (
+            <View style={tailwind("flex flex-row items-center my-4")}>
+              <View
+                style={{
+                  backgroundColor: p.color,
+                  ...tailwind("w-4 h-4 rounded-full mr-2"),
+                }}
+              />
+              <Text>{p.name}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </>
   );
